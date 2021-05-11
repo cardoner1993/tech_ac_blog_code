ceb31aca4211ddbc15b733dda00b174caa2b76e5
dcardoner <david.cardoner@redstor.com>
2021-05-11 12:24:05 +0200

    Add fibonacci examples to code

--- numba_examples.py
+++ numba_examples.py
@@ -1,7 +1,9 @@
 import numpy as np
-import numba
 from functools import wraps
 from time import process_time
+import timeit
+from numba import njit, jit
+
 
 def measure(func):
     @wraps(func)
@@ -17,8 +19,27 @@ def measure(func):
 
     return _time_it
 
-# np zeros_like is not accepted by numba
-@measure
+
+def fibonacci(n):
+    if n < 2:
+        return n
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+# print(f"Running fibonacci. Took {timeit.timeit('fibonacci(35)', 'from __main__ import fibonacci', number=30)}")
+
+
+@jit(fastmath=True, cache=True, nopython=True)
+def fibonacci_numba(n):
+    if n < 2:
+        return n
+    else:
+        return fibonacci_numba(n - 1) + fibonacci_numba(n - 2)
+
+# print(f"Running fibonacci numba. Took "
+#       f"{timeit.timeit('fibonacci_numba(35)', 'from __main__ import fibonacci_numba', number=30)}")
+
+
 def search_min_np(grid):
     mins = np.zeros_like(grid, dtype=bool)
     for i in range(1, grid.shape[1] - 1):
@@ -42,9 +63,7 @@ def search_min_np_jit(grid):
     return np.nonzero(mins)
 
 
-# Option accepted by numba the only thing is that we need to move the zeros_like outside the function
-# Also the decorator is not allowed in terms of f strings and use of kwargs because are dictionaries
-@numba.jit(nopython=True)
+@jit(nopython=True)
 def _search_min(grid, mins):
     for i in range(1, grid.shape[1] - 1):
         for j in range(1, grid.shape[0] - 1):
@@ -59,17 +78,46 @@ def _search_min(grid, mins):
                 mins[i, j] = True
 
 
+# Option accepted by numba the only thing is that we need to move the zeros_like outside the function
+# Also the decorator is not allowed in terms of f strings and use of kwargs because are dictionaries
+
+
 if __name__ == '__main__':
-    data = np.random.randn(2000, 2000)
+    # data = np.random.randn(2000, 2000)
 
-    print("Eval function search_min_np")
-    search_min_np(data)
+    #     SETUP_CODE = '''
+    # from __main__ import search_min_np
+    # import numpy as np'''
+    #
+    #     TEST_CODE = '''
+    # data = np.random.randn(2000, 2000)'''
+    #
+    #     print("Eval function search_min_np")
+    #     print(timeit.timeit(setup=SETUP_CODE, stmt=TEST_CODE, number=30))
+    #     # measure(search_min_np(data))
+    #
+    #     SETUP_CODE = '''
+    # import numpy as np
+    # from numba import njit, jit
+    # from __main__ import search_min_np
+    # jit(nopython=True)(search_min_np)
+    # '''
+    #
+    #     TEST_CODE = '''
+    # data = np.random.randn(2000, 2000)'''
+    #
+    #     try:
+    #         print("Eval function search_min_np with jit decorator")
+    #         find_min_jit = jit(nopython=True)(search_min_np)
+    #         # print(timeit.timeit(setup=SETUP_CODE, stmt=TEST_CODE, number=30))
+    #         find_min_jit(data)
+    #     except Exception as e:
+    #         print(f"Failed executing find_min_jit. Reason {str(e)}")
+    #
+    #     print("Eval function search_min_np_jit")
+    #     search_min_np_jit(data)
 
-    try:
-        print("Eval function search_min_np with jit decorator")
-        find_min_jit = numba.jit(nopython=True)(search_min_np)
-        find_min_jit(data)
-    except Exception as e:
-        print(f"Failed executing find_min_jit. Reason {str(e)}")
-
-    search_min_np_jit(data)
+    print("Running fibonacci")
+    print(timeit.timeit("fibonacci(35)", "from __main__ import fibonacci", number=30))
+    print("Running fibonacci numba")
+    print(timeit.timeit("fibonacci_numba(35)", "from __main__ import fibonacci_numba", number=30))
